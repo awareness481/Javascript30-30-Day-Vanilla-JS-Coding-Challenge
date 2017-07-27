a62fe741c0685e84cefb5aaec456406170768145
Jesse Jafa <jesse.jafa@gmail.com>
2017-07-28 01:18:56 +0300

    Countdown Timer Initial Commit

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,55 +1,77 @@
 let countdown;
-const timerDisplay = document.querySelector('.display__time-left');
+const displayTimer = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
-function timer(seconds) {
-  // clear any existing timers
+/**
+ * [timer description]
+ * @param  {[type]} secs Seconds passed by the user
+ * @return {[type]}      [description]
+ */
+function timer(secs) {
+  /**
+   * Clearing any existing intervals
+   * @param  {[type]} countdown [description]
+   * @return {[type]}           [description]
+   */
   clearInterval(countdown);
 
   const now = Date.now();
-  const then = now + seconds * 1000;
-  displayTimeLeft(seconds);
+  const then = now + secs * 1000;
+  displayTimeLeft(secs);
   displayEndTime(then);
 
   countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
-    if(secondsLeft < 0) {
+    const secsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (secsLeft < 0) {
       clearInterval(countdown);
       return;
     }
-    // display it
-    displayTimeLeft(secondsLeft);
+    displayTimeLeft(secsLeft);
   }, 1000);
 }
 
-function displayTimeLeft(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+/**
+ * [displayTimeLeft display timer
+ * @param  {[type]} secs seconds given
+ * @return {[type]}      [description]
+ */
+function displayTimeLeft(secs) {
+  const mins = Math.floor(secs / 60);
+  const remainderSecs = secs % 60;
+  const display = `${mins}:${remainderSecs < 10 ? '0' : ''}${remainderSecs}`;
+
   document.title = display;
-  timerDisplay.textContent = display;
+  displayTimer.textContent = display;
 }
 
+/**
+ * display when users should be back
+ * @param  {[type]} timestamp Unix time
+ * @return {[type]}           [description]
+ */
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
-  const adjustedHour = hour > 12 ? hour - 12 : hour;
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+
+  endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
+/**
+ * Starts countdown
+ * @return {[type]} [description]
+ */
 function startTimer() {
-  const seconds = parseInt(this.dataset.time);
-  timer(seconds);
+  const secs = parseInt(this.dataset.time, 10);
+  timer(secs);
 }
-
 buttons.forEach(button => button.addEventListener('click', startTimer));
+
 document.customForm.addEventListener('submit', function(e) {
   e.preventDefault();
   const mins = this.minutes.value;
-  console.log(mins);
   timer(mins * 60);
   this.reset();
 });
